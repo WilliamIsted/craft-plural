e31225b68d4b1fba7f8f5fb15b002de701337f8d
William Isted <william@isted.me>
2016-05-08 14:06:46 +0100

    Initial Release
    - Description on plugin page displays reminder on usage in the plugin description.
    - Documentation link added.
    - Release feed added, allowing Craft to notify users of updates to the plugin.

--- plural/PluralPlugin.php
+++ plural/PluralPlugin.php
@@ -11,7 +11,7 @@ class PluralPlugin extends BasePlugin {
 	}
 
 	public function getVersion() {
-		return '0.1.0';
+		return '1.0.0';
 	}
 
 	public function getDeveloper() {
@@ -20,6 +20,22 @@ class PluralPlugin extends BasePlugin {
 
 	public function getDeveloperUrl() {
 		return 'https://william.isted.me';
+	}
+
+	public function getPluginUrl() {
+		return 'https://github.com/WilliamIsted/CraftPlural';
+	}
+
+	public function getDescription() {
+		return 'Usage: {{ entries | length | plural(\'result\') }}';
+	}
+
+	public function getDocumentationUrl() {
+		return $this->getPluginUrl().'/blob/master/README.md';
+	}
+
+	public function getReleaseFeedUrl() {
+		return 'https://raw.githubusercontent.com/WilliamIsted/CraftPlural/master/releases.json';
 	}
 
 	public function addTwigExtension() {
